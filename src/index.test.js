e73469ad8bbc1a99e97cc26e700915b192f76c35
thomasmfx <thomasmoisesf@gmail.com>
2024-08-16 23:21:50 -0300

    style: Add description to tests

--- src/index.test.js
+++ src/index.test.js
@@ -6,67 +6,62 @@ import {
   calculator
 } from "./index";
 
-// capitalize
-test('Returns a string with the first character capitalized', () => {
-  expect(capitalize('pizza is good')).toBe('Pizza is good')
-})
-
-test('Returns a string with the first character capitalized without whitespaces on both ends', () => {
-  expect(capitalize('   pizza is good')).toBe('Pizza is good')
-})
-
-// reverseString
-test('Returns a reversed string', () => {
-  expect(reverseString('I like churros')).toBe('sorruhc ekil I')
-})
-
-test('Returns a reversed string without whitespaces on both ends', () => {
-  expect(reverseString('   I like churros')).toBe('sorruhc ekil I')
-})
-
-// caesarCipher
-test("Caesar cipher: returns correct encryption", () => {
-  expect(caesarCipher('xyz', 3)).toBe('abc');
+describe("capitalize", () => {
+  test('returns capitalized string', () => {
+    expect(capitalize('pizza is good')).toBe('Pizza is good')
+  });
+  test('returns capitalized string without whitespaces', () => {
+    expect(capitalize('   pizza is good')).toBe('Pizza is good')
+  });
 });
 
-test("Caesar cipher: follows original lettercase", () => {
-  expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
+describe("reverseString", () => {
+  test('returns reversed string', () => {
+    expect(reverseString('I like churros')).toBe('sorruhc ekil I')
+  });
+  test('returns reversed string without whitespaces', () => {
+    expect(reverseString('   I like churros')).toBe('sorruhc ekil I')
+  });
 });
 
-test("Caesar cipher: respects punctuation", () => {
-  expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
+describe('caesarCipher', () => {
+  test("returns encrypted string", () => {
+    expect(caesarCipher('xyz', 3)).toBe('abc');
+  });
+  test("follows original lettercase", () => {
+    expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
+  });
+  test("respects punctuation", () => {
+    expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
+  });
 });
-
-//analyzeArray
-test('Returns correctly average value from array', () => {
-  expect(analyzeArray([1,8,3,4,2,6]).average).toEqual(4)
-})
-
-test('Returns correctly minimum value from array', () => {
-  expect(analyzeArray([1,8,3,4,2,6]).min).toEqual(1)
-})
-
-test('Returns correctly max value from array', () => {
-  expect(analyzeArray([1,8,3,4,2,6]).max).toEqual(8)
-})
-
-test('Returns correctly length of array', () => {
-  expect(analyzeArray([1,8,3,4,2,6]).length).toEqual(6)
-})
-
-// calculator
-test('Sums two numbers', () => {
-  expect(calculator.add(5, 10)).toEqual(15);
+  
+describe('analyzeArray', () => {
+  test('computes average value of array', () => {
+    expect(analyzeArray([1,8,3,4,2,6]).average).toEqual(4)
+  });
+  test('computes lowest value in array', () => {
+    expect(analyzeArray([1,8,3,4,2,6]).min).toEqual(1)
+  });
+  test('computes highest value in array', () => {
+    expect(analyzeArray([1,8,3,4,2,6]).max).toEqual(8)
+  });
+  test('computes length of array', () => {
+    expect(analyzeArray([1,8,3,4,2,6]).length).toEqual(6)
+  });
 });
-
-test('Subtract two numbers', () => {
-  expect(calculator.subtract(18, 6)).toEqual(12);
-});
-
-test('Multiply two numbers', () => {
-  expect(calculator.multiply(7, 7)).toEqual(49);
-});
-
-test('Divide two numbers', () => {
-  expect(calculator.divide(21, 3)).toEqual(7);
+  
+describe('calculator', () => {
+  test('sums two numbers', () => {
+    expect(calculator.add(5, 10)).toEqual(15);
+  });
+  test('subtract two numbers', () => {
+    expect(calculator.subtract(18, 6)).toEqual(12);
+  });
+  test('multiply two numbers', () => {
+    expect(calculator.multiply(7, 7)).toEqual(49);
+  });
+  test('divide two numbers', () => {
+    expect(calculator.divide(21, 3)).toEqual(7);
+  });
 });
